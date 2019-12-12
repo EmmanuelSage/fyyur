9790450ceb0e1ad2690bedba9121824a83595e27
Emmanuel Oluyale <xyemmanuel@gmail.com>
2019-12-12 11:27:22 +0100

    add logic for create venue

--- app.py
+++ app.py
@@ -174,14 +174,26 @@ def create_venue_form():
 
 @app.route('/venues/create', methods=['POST'])
 def create_venue_submission():
-  # TODO: insert form data as a new Venue record in the db, instead
-  # TODO: modify data to be the data object returned from db insertion
+  try:
+    # get form data and create 
+    form = VenueForm()
+    venue = Venue(name=form.name.data, city=form.city.data, state=form.state.data, address=form.address.data,
+                  phone=form.phone.data, image_link=form.image_link.data,
+                  genres=form.genres.data, facebook_link=form.facebook_link.data)
+    
+    # commit session to database
+    db.session.add(venue)
+    db.session.commit()
 
-  # on successful db insert, flash success
-  flash('Venue ' + request.form['name'] + ' was successfully listed!')
-  # TODO: on unsuccessful db insert, flash an error instead.
-  # e.g., flash('An error occurred. Venue ' + data.name + ' could not be listed.')
-  # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
+    # flash success 
+    flash('Venue ' + request.form['name'] + ' was successfully listed!')
+  except:
+    # catches errors
+    db.session.rollback()
+    flash('An error occurred. Venue ' + request.form['name'] + ' could not be listed.')
+  finally:
+    # closes session
+    db.session.close()
   return render_template('pages/home.html')
 
 @app.route('/venues/<venue_id>', methods=['DELETE'])
