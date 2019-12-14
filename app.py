ff1b3b29bbad8dfced8b2bc775be32ebe8aa38a6
Emmanuel Oluyale <xyemmanuel@gmail.com>
2019-12-14 23:49:19 +0100

    implement edit for Artist

--- app.py
+++ app.py
@@ -255,27 +255,46 @@ def show_artist(artist_id):
 @app.route('/artists/<int:artist_id>/edit', methods=['GET'])
 def edit_artist(artist_id):
   form = ArtistForm()
-  artist={
-    "id": 4,
-    "name": "Guns N Petals",
-    "genres": ["Rock n Roll"],
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "326-123-5000",
-    "website": "https://www.gunsnpetalsband.com",
-    "facebook_link": "https://www.facebook.com/GunsNPetals",
-    "seeking_venue": True,
-    "seeking_description": "Looking for shows to perform at in the San Francisco Bay Area!",
-    "image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
-  }
-  # TODO: populate form with fields from artist with ID <artist_id>
 
-  return render_template('forms/edit_artist.html', form=form, artist=artist)
+  artist = Artist.query.get(artist_id)
+
+  artist_data = {
+        "id": artist.id,
+        "name": artist.name,
+        "genres": artist.genres,
+        "city": artist.city,
+        "state": artist.state,
+        "phone": artist.phone,
+        "facebook_link": artist.facebook_link,
+        "image_link": artist.image_link
+    }
+
+  return render_template('forms/edit_artist.html', form=form, artist=artist_data)
 
 @app.route('/artists/<int:artist_id>/edit', methods=['POST'])
 def edit_artist_submission(artist_id):
-  # TODO: take values from the form submitted, and update existing
-  # artist record with ID <artist_id> using the new attributes
+  try:
+    form = ArtistForm()
+
+    artist = Artist.query.get(artist_id)
+
+    artist.name = form.name.data
+
+    artist.name = form.name.data
+    artist.phone = form.phone.data
+    artist.state = form.state.data
+    artist.city = form.city.data
+    artist.genres = form.genres.data
+    artist.image_link = form.image_link.data
+    artist.facebook_link = form.facebook_link.data
+    
+    db.session.commit()
+    flash('The Artist ' + request.form['name'] + ' has been successfully updated!')
+  except:
+    db.session.rolback()
+    flash('An Error has occured and the update unsuccessful')
+  finally:
+    db.session.close()
 
   return redirect(url_for('show_artist', artist_id=artist_id))
 
